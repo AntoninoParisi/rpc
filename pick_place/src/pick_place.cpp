072a0ffea56e69b03afa87676e8c2393b490806a
AntoninoParisi <antonioparisi8@yahoo.com>
2021-07-14 17:40:13 +0200

    [ADD] pick re-defined, initialized the mlp3240 ee

--- pick_place/src/pick_place.cpp
+++ pick_place/src/pick_place.cpp
@@ -1,12 +1,10 @@
-// ROS
 #include <ros/ros.h>
-
-// MoveIt
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
 #include <moveit/move_group_interface/move_group_interface.h>
-
-// TF2
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+
+
+float x_pos_obj = 0.25,y_pos_obj = -0.7,z_pos_obj=0.02;
 
 void openGripper(trajectory_msgs::JointTrajectory& posture)
 {
@@ -36,41 +34,36 @@ void closedGripper(trajectory_msgs::JointTrajectory& posture)
   /* Set them as closed. */
   posture.points.resize(1);
   posture.points[0].positions.resize(2);
-  posture.points[0].positions[0] = 0.65;
-  posture.points[0].positions[1] = 0.65;
+  posture.points[0].positions[0] = 0.35;
+  posture.points[0].positions[1] = 0.35;
   posture.points[0].time_from_start = ros::Duration(0.5);
   // END_SUB_TUTORIAL
 }
 
 void pick(moveit::planning_interface::MoveGroupInterface& move_group)
 {
-  // BEGIN_SUB_TUTORIAL pick1
-  // Create a vector of grasps to be attempted, currently only creating single grasp.
-  // This is essentially useful when using a grasp generator to generate and test multiple grasps.
+  // pick1
   std::vector<moveit_msgs::Grasp> grasps;
   grasps.resize(1);
 
   // Setting grasp pose
-  // ++++++++++++++++++++++
-  // This is the pose of panda_link8. |br|
-  // From panda_link8 to the palm of the eef the distance is 0.058, the cube starts 0.01 before 5.0 (half of the length
-  // of the cube). |br|
-  // Therefore, the position for panda_link8 = 5 - (length of cube/2 - distance b/w panda_link8 and palm of eef - some
-  // extra padding)
   grasps[0].grasp_pose.header.frame_id = "robot_base_link";
   tf2::Quaternion orientation;
-  orientation.setRPY(-1.70, -M_PI / 4, -M_PI / 2);
+  //tf2::Quaternion orientation(0,0,0,1);
+
+  orientation.setEuler(M_PI,0,-M_PI);
+  
   grasps[0].grasp_pose.pose.orientation = tf2::toMsg(orientation);
-  grasps[0].grasp_pose.pose.position.x = 0.415;
-  grasps[0].grasp_pose.pose.position.y = 0.0;
-  grasps[0].grasp_pose.pose.position.z = 0.5;
+  grasps[0].grasp_pose.pose.position.x = x_pos_obj;
+  grasps[0].grasp_pose.pose.position.y = y_pos_obj;
+  grasps[0].grasp_pose.pose.position.z = z_pos_obj+0.18;
 
   // Setting pre-grasp approach
   // ++++++++++++++++++++++++++
   /* Defined with respect to frame_id */
   grasps[0].pre_grasp_approach.direction.header.frame_id = "robot_base_link";
   /* Direction is set as positive x axis */
-  grasps[0].pre_grasp_approach.direction.vector.x = 1.0;
+  grasps[0].pre_grasp_approach.direction.vector.z = -1.0;
   grasps[0].pre_grasp_approach.min_distance = 0.095;
   grasps[0].pre_grasp_approach.desired_distance = 0.115;
 
@@ -79,8 +72,8 @@ void pick(moveit::planning_interface::MoveGroupInterface& move_group)
   /* Defined with respect to frame_id */
   grasps[0].post_grasp_retreat.direction.header.frame_id = "robot_base_link";
   /* Direction is set as positive z axis */
-  grasps[0].post_grasp_retreat.direction.vector.z = 1.0;
-  grasps[0].post_grasp_retreat.min_distance = 0.1;
+  grasps[0].post_grasp_retreat.direction.vector.x = -1.0;
+  grasps[0].post_grasp_retreat.min_distance = 0.05;
   grasps[0].post_grasp_retreat.desired_distance = 0.25;
 
   // Setting posture of eef before grasp
@@ -96,7 +89,7 @@ void pick(moveit::planning_interface::MoveGroupInterface& move_group)
 
   // BEGIN_SUB_TUTORIAL pick3
   // Set support surface as table1.
-  move_group.setSupportSurfaceName("table1");
+  //move_group.setSupportSurfaceName("table1");
   // Call pick to pick up the object using the grasps given
   move_group.pick("object", grasps);
   // END_SUB_TUTORIAL
@@ -116,20 +109,23 @@ void place(moveit::planning_interface::MoveGroupInterface& group)
   // +++++++++++++++++++++++++++
   place_location[0].place_pose.header.frame_id = "robot_base_link";
   tf2::Quaternion orientation;
-  orientation.setRPY(0, 0, M_PI / 2);
+
+  orientation.setEuler(M_PI,0,-M_PI);
+
+  
   place_location[0].place_pose.pose.orientation = tf2::toMsg(orientation);
 
   /* While placing it is the exact location of the center of the object. */
-  place_location[0].place_pose.pose.position.x = 0;
-  place_location[0].place_pose.pose.position.y = 0.5;
-  place_location[0].place_pose.pose.position.z = 0.5;
+  place_location[0].place_pose.pose.position.x = -1.05;
+  place_location[0].place_pose.pose.position.y = 0;
+  place_location[0].place_pose.pose.position.z = 0.25;
 
   // Setting pre-place approach
   // ++++++++++++++++++++++++++
   /* Defined with respect to frame_id */
   place_location[0].pre_place_approach.direction.header.frame_id = "robot_base_link";
   /* Direction is set as negative z axis */
-  place_location[0].pre_place_approach.direction.vector.z = -1.0;
+  place_location[0].pre_place_approach.direction.vector.x = -1.0;
   place_location[0].pre_place_approach.min_distance = 0.095;
   place_location[0].pre_place_approach.desired_distance = 0.115;
 
@@ -138,7 +134,7 @@ void place(moveit::planning_interface::MoveGroupInterface& group)
   /* Defined with respect to frame_id */
   place_location[0].post_place_retreat.direction.header.frame_id = "robot_base_link";
   /* Direction is set as negative y axis */
-  place_location[0].post_place_retreat.direction.vector.y = -1.0;
+  place_location[0].post_place_retreat.direction.vector.z = -1.0;
   place_location[0].post_place_retreat.min_distance = 0.1;
   place_location[0].post_place_retreat.desired_distance = 0.25;
 
@@ -148,7 +144,7 @@ void place(moveit::planning_interface::MoveGroupInterface& group)
   openGripper(place_location[0].post_place_posture);
 
   // Set support surface as table2.
-  group.setSupportSurfaceName("table2");
+  //group.setSupportSurfaceName("table2");
   // Call place to place the object using the place locations given.
   group.place("object", place_location);
   // END_SUB_TUTORIAL
@@ -162,72 +158,27 @@ void addCollisionObjects(moveit::planning_interface::PlanningSceneInterface& pla
   // ^^^^^^^^^^^^^^^^^^^^
   // Create vector to hold 3 collision objects.
   std::vector<moveit_msgs::CollisionObject> collision_objects;
-  collision_objects.resize(3);
-
-  // Add the first table where the cube will originally be kept.
-  collision_objects[0].id = "table1";
+  collision_objects.resize(1);
+  // Define the object that we will be manipulating
   collision_objects[0].header.frame_id = "robot_base_link";
+  collision_objects[0].id = "object";
 
   /* Define the primitive and its dimensions. */
   collision_objects[0].primitives.resize(1);
   collision_objects[0].primitives[0].type = collision_objects[0].primitives[0].BOX;
   collision_objects[0].primitives[0].dimensions.resize(3);
   collision_objects[0].primitives[0].dimensions[0] = 0.2;
-  collision_objects[0].primitives[0].dimensions[1] = 0.4;
-  collision_objects[0].primitives[0].dimensions[2] = 0.4;
+  collision_objects[0].primitives[0].dimensions[1] = 0.05;
+  collision_objects[0].primitives[0].dimensions[2] = 0.1;
 
-  /* Define the pose of the table. */
+  /* Define the pose of the object. */
   collision_objects[0].primitive_poses.resize(1);
-  collision_objects[0].primitive_poses[0].position.x = 0.5;
-  collision_objects[0].primitive_poses[0].position.y = 0;
-  collision_objects[0].primitive_poses[0].position.z = 0.2;
+  collision_objects[0].primitive_poses[0].position.x = x_pos_obj;
+  collision_objects[0].primitive_poses[0].position.y = y_pos_obj;
+  collision_objects[0].primitive_poses[0].position.z = z_pos_obj;
   // END_SUB_TUTORIAL
 
   collision_objects[0].operation = collision_objects[0].ADD;
-
-  // BEGIN_SUB_TUTORIAL table2
-  // Add the second table where we will be placing the cube.
-  collision_objects[1].id = "table2";
-  collision_objects[1].header.frame_id = "robot_base_link";
-
-  /* Define the primitive and its dimensions. */
-  collision_objects[1].primitives.resize(1);
-  collision_objects[1].primitives[0].type = collision_objects[1].primitives[0].BOX;
-  collision_objects[1].primitives[0].dimensions.resize(3);
-  collision_objects[1].primitives[0].dimensions[0] = 0.4;
-  collision_objects[1].primitives[0].dimensions[1] = 0.2;
-  collision_objects[1].primitives[0].dimensions[2] = 0.4;
-
-  /* Define the pose of the table. */
-  collision_objects[1].primitive_poses.resize(1);
-  collision_objects[1].primitive_poses[0].position.x = 0;
-  collision_objects[1].primitive_poses[0].position.y = 0.5;
-  collision_objects[1].primitive_poses[0].position.z = 0.2;
-  // END_SUB_TUTORIAL
-
-  collision_objects[1].operation = collision_objects[1].ADD;
-
-  // BEGIN_SUB_TUTORIAL object
-  // Define the object that we will be manipulating
-  collision_objects[2].header.frame_id = "robot_base_link";
-  collision_objects[2].id = "object";
-
-  /* Define the primitive and its dimensions. */
-  collision_objects[2].primitives.resize(1);
-  collision_objects[2].primitives[0].type = collision_objects[1].primitives[0].BOX;
-  collision_objects[2].primitives[0].dimensions.resize(3);
-  collision_objects[2].primitives[0].dimensions[0] = 0.02;
-  collision_objects[2].primitives[0].dimensions[1] = 0.02;
-  collision_objects[2].primitives[0].dimensions[2] = 0.2;
-
-  /* Define the pose of the object. */
-  collision_objects[2].primitive_poses.resize(1);
-  collision_objects[2].primitive_poses[0].position.x = 0.5;
-  collision_objects[2].primitive_poses[0].position.y = 0;
-  collision_objects[2].primitive_poses[0].position.z = 0.5;
-  // END_SUB_TUTORIAL
-
-  collision_objects[2].operation = collision_objects[2].ADD;
 
   planning_scene_interface.applyCollisionObjects(collision_objects);
 }
@@ -247,12 +198,12 @@ int main(int argc, char** argv)
   addCollisionObjects(planning_scene_interface);
   ROS_INFO("Before pick");
   // Wait a bit for ROS things to initialize
-  ros::WallDuration(1.0).sleep();
+  ros::WallDuration(3.0).sleep();
   ROS_INFO("Picking...");
   pick(group);
   ROS_INFO("After pick");
 
-  ros::WallDuration(1.0).sleep();
+  ros::WallDuration(3.0).sleep();
   ROS_INFO("Placing...");
 
   place(group);
